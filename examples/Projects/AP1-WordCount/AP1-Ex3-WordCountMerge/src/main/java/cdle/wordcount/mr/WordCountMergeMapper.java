df7657fe9a1b0b2da1bb6d361f5fe10156043484
AnaSofOliveira <anasofiaoliveira.workmail@gmail.com>
2024-10-25 17:15:28 +0100

    🔧 Aula Prática 1

--- examples/Projects/AP1-WordCount/AP1-Ex3-WordCountMerge/src/main/java/cdle/wordcount/mr/WordCountMergeMapper.java
+++ examples/Projects/AP1-WordCount/AP1-Ex3-WordCountMerge/src/main/java/cdle/wordcount/mr/WordCountMergeMapper.java
@@ -1,8 +1,6 @@
 package cdle.wordcount.mr;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
